2907f4ed5c0748e2699fbca7843bed32ecf67af8
your_github_username <your_github_email>
2020-04-02 16:01:17 -0600

    Nueva prueba deploy

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Mostrando el deploy
+          Nueva prueba
         </a>
       </header>
     </div>

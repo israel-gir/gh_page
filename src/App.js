b1af8fa38aa51950de22b935bb42e095b765c610
Israel Moreno <se.israel.mc@gmail.com>
2020-04-03 09:58:58 -0600

    Test Deploy

--- src/App.js
+++ src/App.js
@@ -7,16 +7,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Working <code> GIR </code> Complete deploy :D
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Nueva prueba
+          Deploy App with Github Pages
         </a>
       </header>
     </div>
